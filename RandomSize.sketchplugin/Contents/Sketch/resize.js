3b904d33be9544d6e544938b9f31dd0cc7e9681e
Long Zheng <long.zheng@gmail.com>
2020-08-01 14:49:07 +1000

    Update for Sketch

--- RandomSize.sketchplugin/Contents/Sketch/resize.js
+++ RandomSize.sketchplugin/Contents/Sketch/resize.js
@@ -1,61 +1,84 @@
-function onRun(context) {
+const sketch = require("sketch");
 
-  var sketch = context.api()
-  var selection = context.selection;
-  var doc = context.document;
+var onRun = function (context) {
+  const doc = sketch.getSelectedDocument();
+  const selectedLayers = doc.selectedLayers;
 
-  var options = ['Width', 'Height'];
-  var choice = createSelect('Randomise width or height?', options, 0);
-
-  // if dropdown prompt cancelled
-  if (choice[0] == 1001)
+  if (selectedLayers.length === 0) {
+    sketch.UI.message(`No layers selected`);
     return;
-
-  var maxSize = [doc askForUserInput:"Upper size?" initialValue:"0"];
-  var minSize = [doc askForUserInput:"Lower size?" initialValue:"0"];
-
-  for (var i = 0; i < selection.length; i++) {
-    var layer = selection[i];
-    var randomSize = getRandomInt(minSize, maxSize);
-
-    switch (choice[1]) {
-      case 0: // width
-        layer.frame().setWidth(randomSize);
-        break;
-      case 1: // height
-        var bottomY = layer.frame().y() + layer.frame().height(); // bottom Y position
-        layer.frame().setHeight(randomSize);
-        layer.frame().setY(bottomY - randomSize); // simulate align to bottom
-        break;
-    }
   }
-  
-}
 
-// Create dropdown modal
-function createSelect(msg, items, selectedItemIndex) {
-  selectedItemIndex = selectedItemIndex || 0
+  let widthOrHeight = "";
+  let maxSize = 0;
+  let minSize = 0;
 
-  var accessory = [[NSComboBox alloc] initWithFrame:NSMakeRect(0, 0, 200, 25)]
-  [accessory addItemsWithObjectValues: items]
-    [accessory selectItemAtIndex:selectedItemIndex]
+  getWidthOrHeight()
+    .then((result) => {
+      widthOrHeight = result;
+      return getSize("Maximum size?", 100);
+    })
+    .then((result) => {
+      maxSize = result;
+      return getSize("Minimum size?", 0);
+    })
+    .then((result) => {
+      this.minSize = result;
 
-  var alert = [[NSAlert alloc] init]
-  [alert setMessageText: msg]
-    [alert addButtonWithTitle:'OK']
-    [alert addButtonWithTitle:'Cancel']
-    [alert setAccessoryView:accessory]
+      selectedLayers.forEach((layer) => {
+        let randomSize = getRandomInt(minSize, maxSize);
 
-  var responseCode = [alert runModal]
-  var sel = [accessory indexOfSelectedItem]
+        switch (widthOrHeight) {
+          case "Width":
+            layer.frame.width = randomSize;
+            break;
+          case "Height":
+            var bottomY = layer.frame.y + layer.frame.height; // bottom Y position
+            layer.frame.height = randomSize;
+            layer.frame.y = bottomY - randomSize; // simulate align to bottom
+            break;
+        }
+      });
+    }).catch((e) => {
+      console.log(e)
+    });
+};
 
-  return [responseCode, sel]
-}
+var getWidthOrHeight = () => {
+  return new Promise((resolve, reject) => {
+    sketch.UI.getInputFromUser(
+      "Randomise width or height?",
+      {
+        type: sketch.UI.INPUT_TYPE.selection,
+        possibleValues: ["Width", "Height"],
+      },
+      (err, value) => {
+        if (err) reject(err);
+        resolve(value);
+      }
+    );
+  });
+};
+
+var getSize = (prompt, initialValue) => {
+  return new Promise((resolve, reject) => {
+    sketch.UI.getInputFromUser(
+      prompt,
+      {
+        initialValue: initialValue,
+      },
+      (err, value) => {
+        if (err) reject(err);
+        resolve(value);
+      }
+    );
+  });
+};
 
 // Returns a random integer between min (included) and max (excluded)
 // Using Math.round() will give you a non-uniform distribution!
-function getRandomInt(min, max) {
+var getRandomInt = (min, max) => {
   var min = parseInt(min);
   var max = parseInt(max);
-  return Math.round((Math.random() * (max - min) + min), 0);
-}
+  return Math.round(Math.random() * (max - min) + min, 0);
+};
